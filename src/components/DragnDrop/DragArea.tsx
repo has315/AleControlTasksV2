4980cde4f8a84ae3c3350b0e928946315749cc6b
has315 <arsen_sandic@hotmail.com>
2021-03-10 03:52:10 +0100

    finishing DragArea

--- src/components/DragnDrop/DragArea.tsx
+++ src/components/DragnDrop/DragArea.tsx
@@ -12,8 +12,6 @@ const DragArea: React.FC<DragProps> = ({ items, onChange, children }: DragProps)
         let tmp = arr[index2];
         arr[index2] = arr[index1];
         arr[index1] = tmp
-        
-        // return [arr[index1], arr[index2]] = [arr[index2], arr[index1]]
         return [...arr]
     }
 
